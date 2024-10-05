5eb94764d977fd38b2ec02716a749628635accb8
NeerajQA <neeraj1264qa@gmail.com>
2024-10-05 15:28:18 +0530

    3:28

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -56,6 +56,7 @@ const Gallery = () => {
                     loop
                     muted
                     playsInline
+                    controls
                   />
                 </div>
               ) : (
